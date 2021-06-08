4ffecce6a780182d4535084b60a67227f8a38569
al-uminium <79945949+al-uminium@users.noreply.github.com>
2021-06-09 04:12:44 +0800

    cleaned code

--- library/script.js
+++ library/script.js
@@ -1,12 +1,31 @@
-const myLibrary = []
+const myLibrary = [
+    {
+        "author": "Chinua Achebe",
+        "country": "Nigeria",
+        "imageLink": "images/things-fall-apart.jpg",
+        "language": "English",
+        "link": "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
+        "pages": 209,
+        "title": "Things Fall Apart",
+        "year": 1958
+      },
+      {
+        "author": "Hans Christian Andersen",
+        "country": "Denmark",
+        "imageLink": "images/fairy-tales.jpg",
+        "language": "Danish",
+        "link": "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n",
+        "pages": 784,
+        "title": "Fairy tales",
+        "year": 1836
+      }
+]
 
 //Query Selectors
 const openForm = document.querySelector("#open-form")
 const formContainer = document.querySelector(".form-container");
 const closeForm = document.querySelector("#close-form")
 const addBook = document.querySelector("#add-book")
-const delBook = document.getElementsByClassName("remove-book")
-// const readBook = document.querySelector("")
 
 //Book constructor and methods
 function Book(title, author, pages, perused) {
@@ -16,12 +35,6 @@ function Book(title, author, pages, perused) {
     this.perused = perused; //bool val
 }
 
-Book.prototype.info = function() {
-    let read = (this.perused) ? "have read" : "have not read"
-    return `${this.title} by ${this.author}, ${read}`
-}
-
-
 //Helper functions
 const createBook = () => {
     let bookTitle = document.querySelector("#book-title").value;
@@ -29,40 +42,48 @@ const createBook = () => {
     let bookPages = document.querySelector("#book-pages").value;
     let perused = document.querySelector("#book-read").checked;
     
+    if (!bookTitle || !bookAuthor || !bookPages) {
+        return null
+    }
+    
     return new Book(bookTitle, bookAuthor, bookPages, perused)
 }
 
-const renderBooks = (title, author, pages, read) => {
+const renderBooks = () => {
+    let libraryCont = document.querySelector("#library-container")
     const renderBookCards = (title, author, pages, read, index) => {
-        let libraryCont = document.querySelector("#library-container")
-        if (read) {
-            libraryCont.innerHTML += `
-                <div class=\"book-card\" id=\"${index}\">
-                    <p class=\"book-title\">${title}</p>
-                    <p class=\"book-author\">${author}</p>
-                    <p class=\"book-pages\">Pages: ${pages}</p>
-                    <div>
-                        <input class=\"read-book\" type=\"button\" value=\"Read\">
-                        <input class=\"remove-book\" type=\"button\" value=\"Remove Book\">
-                    </div>
+        let buttonValue = (read) ? "Read" : "Have not read"
+        libraryCont.innerHTML += `
+            <div class=\"book-card\">
+                <p class=\"book-title\">${title}</p>
+                <p class=\"book-author\">${author}</p>
+                <p class=\"book-pages\">Pages: ${pages}</p>
+                <div class=\"book-btns\">
+                    <input class=\"read-book\" type=\"button\" value=\"${buttonValue}\">
+                    <input id=\"${index}\" class=\"remove-book\" type=\"button\" value=\"Remove Book\">
                 </div>
+            </div>
             `
-        } else {
-            libraryCont.innerHTML += `
-                <div class=\"book-card\" id=\"${index}\">
-                    <p class=\"book-title\">${title}</p>
-                    <p class=\"book-author\">${author}</p>
-                    <p class=\"book-pages\">Pages: ${pages}</p>
-                    <div>
-                        <input type=\"button\" value=\"To Read\">
-                        <input type=\"button\" value=\"Remove Book\">
-                    </div>
-                </div>
-            `
-        }
     }
+
+    libraryCont.innerHTML = ""
+
     for (let i = 0; i < myLibrary.length; i++) {
-        renderBookCards(title, author, pages, read, i)
+        renderBookCards(myLibrary[i].title, 
+            myLibrary[i].author, 
+            myLibrary[i].pages, 
+            myLibrary[i].read, 
+            i)
+    }
+
+    const delBook = document.getElementsByClassName("remove-book")
+    const readBook = document.getElementsByClassName("read-book")
+
+    for (const btn of delBook) {
+        btn.addEventListener("click", (e) => delBookFromLibrary(e))
+    }
+    for (const btn of readBook) {
+        btn.addEventListener("click", (e) => console.log(e))
     }
 }
 
@@ -77,40 +98,39 @@ const checkForDuplicate = (title, author) => {
 
 const addBookToLibrary = (event) => {
     event.preventDefault() //prevents form submission & page reloading
-    const book = createBook(event);
 
-    let title = book.title;
-    let author = book.author
-    let pages = book.pages
-    let read = book.perused
+    const book = createBook(event)
 
-    if (checkForDuplicate(title, author)) { //returns true if there is duplicate
-        alert("You already have this book in your library")
-        return
+    if (book) {
+        if (checkForDuplicate(book.title, book.author)) { //returns true if there is duplicate
+            alert("You already have this book in your library")
+            return
+        }
+
+        myLibrary.push(book)
+        renderBooks()
+    
+        document.querySelector("#book-title").value = ""
+        document.querySelector("#book-author").value = ""
+        document.querySelector("#book-pages").value = ""
+        document.querySelector("#book-read").checked = false;
     }
-    myLibrary.push(book)
 
+}
 
-    renderBooks(title,author,pages,read)
+const changeRead = (e) => {
 
-    document.querySelector("#book-title").value = ""
-    document.querySelector("#book-author").value = ""
-    document.querySelector("#book-pages").value = ""
-    document.querySelector("#book-read").checked = false;
 }
 
 const delBookFromLibrary = (e) => {
-    //this function will fire upon clicking of remove button
-    //will take target id, run splice(idnum, 1)
-    //once target is removed, run render function
-    console.log(e.target.id)
+    myLibrary.splice(e.target.id,1)
+    renderBooks()
 }
 
-const showForm = () => { //creates empty div, set #cover-div id, and append to body
+const showForm = () => {
     const newDiv = document.createElement("div");
     newDiv.id = "cover-div";
     document.body.appendChild(newDiv);
-
     formContainer.style.display = "block";
 }
 
