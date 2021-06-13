ce23658731283caa81a7bfd9f8f1a95cbba443e5
al-uminium <79945949+al-uminium@users.noreply.github.com>
2021-06-13 19:41:58 +0800

    FIXED IT ALL

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -26,7 +26,7 @@ const AIPlayer = () => {
         }
         return aiChoices[Math.floor(Math.random() * aiChoices.length)]
     }
-    return {aiStatus, aiTurn}
+    return {aiPlayer, aiStatus, aiTurn}
 }
 
 const Player = (X) => {
@@ -54,13 +54,13 @@ const gameBoard = () => {
 
     const updateState = (e, player) => {
         if (!e.target.textContent) {
-            e.target.textContent = player //update text
-            gameState[e.target.id] = player //update array
+            e.target.innerHTML += `<p class="player-anim">${player}</p>` 
+            gameState[e.target.id] = player 
         } 
     }
 
-    const aiUpdateState = (aiSelect, player) => {
-        boards[aiSelect].textContent = player
+    const aiUpdateState = (boards, aiSelect, player) => {
+        boards[aiSelect].innerHTML += `<p class="comp-anim">${player}</p>`
         gameState[aiSelect] = player
     }
 
@@ -76,19 +76,17 @@ const gameBoard = () => {
             ["0","4","8"]
         ]
 
-        if (!arr.includes("")) { //if all squares are filled, return null
-            return "draw"
-
-        } else if (arr.includes("")) {
-            for (const winCase of winCases) {
-                let [a, b, c] = winCase;
-                if (arr[a].length && arr[a] === arr[b] && arr[a] === arr[c]) {
-                    return true
-                }
+        for (const winCase of winCases) {
+            let [a, b, c] = winCase;
+            if (arr[a].length && arr[a] === arr[b] && arr[a] === arr[c]) {
+                return [true, arr[a], [a,b,c]]
             }
         }
 
-        return false
+        if (!arr.includes("")) { //if all squares are filled, return "draw"
+            return ["draw"]
+        } 
+        return [false]
     }
     return {gameState, 
         isValidMove, 
@@ -104,59 +102,71 @@ const Game = () => {
     const O = document.querySelector("#O")
     const winnerMessage = document.querySelector("#winner-message")
     const replayBtn = document.querySelector("#replay")
+    const playWithFriend = document.querySelector("#friend-play")
 
     let game = gameBoard()
     let player = Player(X)
     let ai = AIPlayer()
     let gameActive = true;
 
-    const gameFinished = (game, player) => {
-        switch (game) {
+    const gameFinished = (game, aiON) => {
+        let opponent = (aiON) ? "Computer won!" : "Your friend won!"
+        let winner = ""
+        if (X.checked) {
+            winner = (game[1] === "X") ? "You won!" : opponent
+        }
+        if (O.checked) {
+            winner = (game[1] === "O") ? "You won!" : opponent
+        }
+        switch (game[0]) {
             case true:
-                let winner = (X.checked && player.choice==="X" || O.checked && player.choice==="O") ? "You won!" : "Computer won!"
                 winnerMessage.textContent = winner;
                 replayBtn.style.display = "block"
-            case false:
-                //code
+                for (const val of game[2]){
+                    boards[val].childNodes[0].classList.add("win-anim")
+                }
+                break;
             case "draw":
-                //code
+                winnerMessage.textContent = "It's a draw!";
+                replayBtn.style.display = "block"
+                break
         }
-        
     }
     
     const playRound = (game, player, e) => {
-        //isValidMove() changes arr values for gamestate and returns true if the state *has* been changed, else returns false 
+        //isValidMove() checks if target has text content, is yes, return false.
         let AITurnedOn = ai.aiStatus()
         let gameWon = game.checkGame(game.gameState)
 
         if (gameActive) {
             if (game.isValidMove(e)) { 
                 game.updateState(e, player.choice);
-                switch (gameWon) {
+                gameWon = game.checkGame(game.gameState) //need to check if game won after you click
+                if (!gameWon[0] && AITurnedOn) {
+                    let aiSelection = ai.aiTurn(game.gameState)
+                    if (aiSelection) {
+                        game.aiUpdateState(boards, aiSelection, player.computer)
+                        gameWon = game.checkGame(game.gameState) //check if game won after computer's turn
+                    }
+                }
+                switch (gameWon[0]) {
                     case false:
+                        if (AITurnedOn) {
+                            break
+                        }
                         player.nextTurn(player);
+                        break
                     case true:
                         gameActive = false;
-                        gameFinished(player)
+                        break
                     case "draw":
-
+                        gameActive = false;
+                        break
                 }
-                if (AITurnedOn) {
-                    let aiSelection = ai.aiTurn(game.gameState)
-                    aiUpdateState(aiSelection, player.choice)
-                }
-                
-                // gameActive = (game.checkGame(game.gameState)) ? false : player.nextTurn(player);
-                // if (AITurnedOn) {
-                    
-                //     game.isValidMove(aiSelection, player.choice)
-                //     boards[aiSelection].textContent = player.choice
-                //     player.nextTurn(player)
-                // }
             } 
         }
         if (!gameActive) {
-            gameFinished(player);
+            gameFinished(gameWon, AITurnedOn)
             return
         }
     }
@@ -181,6 +191,10 @@ const Game = () => {
     replayBtn.addEventListener("click", () => replayGame())
     X.addEventListener("click", () => replayGame())
     O.addEventListener("click", () => replayGame())
+    playWithFriend.addEventListener("click", () => replayGame())
+    ai.aiPlayer.addEventListener("click", () => replayGame())
+    
+    return {gameActive}
 }
 
 Game()
